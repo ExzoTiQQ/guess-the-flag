933b13396770d23e45981c4d4cd273b7f384a5d4
Yaroslav Afenkin <exzotiqnexus@gmail.com>
2018-01-25 18:53:14 +0100

    Added button class

--- src/CoutryPick.js
+++ src/CoutryPick.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import shuffle from 'shuffle-array';
 
+import './CountryPick.css';
+
 const CountryPick = ({choices, onClick}) => {
     choices = shuffle(choices);
     if(choices.length === 4) {
         var names = choices.map(country => country.name);
         var buttons = names.map(name => (
-            <button onClick={onClick} key={name} type="button">{name}</button>
+            <button className="choice" onClick={onClick} key={name} type="button">{name}</button>
         ));
     }
     return (
