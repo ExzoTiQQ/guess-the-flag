881eeeb2aef3d9c93297a0b19905ca2d69685a68
Yaroslav Afenkin <exzotiqnexus@gmail.com>
2018-01-25 16:24:55 +0100

    Rendering buttons to pick country

--- src/CoutryPick.js
+++ src/CoutryPick.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const CountryPick = ({choices}) => {
-    return <div>CountryPick</div>
+    console.log(choices);
+    if(choices.length === 4) {
+        var names = choices.map(country => country.name);
+        var buttons = names.map(name => (
+            <button key={name} type="button">{name}</button>
+        ));
+    }
+    return (
+        <div>
+            {buttons}
+        </div>
+    )
 }
 
 export default CountryPick;
