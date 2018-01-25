9a0c7f5a7d30f79c52da9a66cb8c8af6088ee09b
Yaroslav Afenkin <exzotiqnexus@gmail.com>
2018-01-25 17:53:51 +0100

    Implemented all game functionality. todo: dry the code up

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ class App extends Component {
     this.state = {
       countries: [],
       currentCountry: {},
-      choices: []
+      choices: [],
+      guessMade: false,
+      result: false
     }
     this.handleChoice = this.handleChoice.bind(this);
+    this.handleContinue = this.handleContinue.bind(this);
   }
 
   componentDidMount() {
@@ -29,25 +32,39 @@ class App extends Component {
         function getRandomCountry () {
           return countries[Math.floor(Math.random() * countries.length)]
         }
-        this.setState({countries, currentCountry, choices}, () => console.log(this.state));
+        this.setState({countries, currentCountry, choices});
       });
   }
 
   handleChoice(e) {
     var clicked = e.target.innerHTML;
-    console.log(clicked, this.state.currentCountry);
+    var result;
+    result = clicked === this.state.currentCountry.name;
+    this.setState({result, guessMade: true});
+  }
+
+  handleContinue() {
+    var {countries} = this.state;
+    var currentCountry = getRandomCountry();
+    var choices = [currentCountry, getRandomCountry(), getRandomCountry(), getRandomCountry()];
+    function getRandomCountry () {
+      return countries[Math.floor(Math.random() * countries.length)]
+    }
+    this.setState({currentCountry, choices, guessMade: false});
   }
 
   render() {
-    
-
-    return (
+      return (
       <div className="App">
         <h1>Guess the flag</h1>
-        <Flag flagUrl={this.state.currentCountry ? this.state.currentCountry.flag : ""} />
-        {this.state.choices.length > 0 ? <CountryPick choices={this.state.choices} onClick={this.handleChoice}/> : null}
+        {this.state.guessMade === false 
+            ? <Flag flagUrl={this.state.currentCountry.flag || ""} /> 
+            : this.state.result 
+              ? <div><h2>You guessed! {this.state.currentCountry.name}</h2><button onClick={this.handleContinue}>Continue?</button></div>
+              : <div><h2>You didn't guess! Correct: {this.state.currentCountry.name}</h2><button onClick={this.handleContinue}>Continue?</button></div>}
+        {!this.state.guessMade && this.state.choices.length > 0 ? <CountryPick choices={this.state.choices} onClick={this.handleChoice}/> : null}
       </div>
-    );
+      );
   }
 }
 
