8869e5adf5ef9b06e26df7a7f4bfce510410d99e
Yaroslav Afenkin <exzotiqnexus@gmail.com>
2018-01-25 15:50:56 +0100

    Fetching country name and flag successfully and setting the state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then(data => data.json())
+      .then(data => {
+        var countries = data.map(country => (
+          {
+            name: country.name,
+            flag: country.flag
+          }
+        ));
+        this.setState({countries});
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <h1>Guess the flag</h1>
+        {/* <Flag flagUrl={}/> */}
+        {/* <CountryPick choices={}/> */}
       </div>
     );
   }
